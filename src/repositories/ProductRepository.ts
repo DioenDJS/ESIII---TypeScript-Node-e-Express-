14b808f358429d4e953471dd29b6c17b64786d63
DioenDJS <negociospacheti@gmail.com>
2021-09-18 23:15:04 -0300

    :art: Add method alterar/delete class ProductRepositor

--- src/repositories/ProductRepository.ts
+++ src/repositories/ProductRepository.ts
@@ -6,44 +6,50 @@ export default class ProductRepository{
     constructor(){
         this.products = []
     }
-
     public findAll():Product[]{
         return this.products;
     }
-
+    
     public findByCode(code: number): Product | undefined{
-        return this.products.find(item => item.code === code);
+        return this.products.find(item => item.code == code);
     }
 
     public save({code, description, buyPrice, sellPrice, tags, lovers}: Product){
         const product = new Product({code, description, buyPrice, sellPrice, tags, lovers});
         this.products.push(product)
-
+        console.log(this.products)
         return product;
     }
 
-    public alterar({lovers, buyPrice, sellPrice, tags, id}: Product): Product[] {
+    public alterar({ description, code, buyPrice, sellPrice, tags, id}: Product): Product {
+    
+        const productsChange = this.products.find((value) => value.id === id);
+        const storeValuablesProductsChange = productsChange;
+    
+    console.log(id)
+    console.log(productsChange)
 
-        const productsChange = this.products.filter(item => {
+        if(!productsChange){
+            throw Error('Produto já existe ! ');
+        }
+        const newListProducts = this.products.filter(value => value.id !== id);
 
-            
-                if(item.id === id){
-                    return item
-                }else{
-                    item.sellPrice = sellPrice
-                    item.description = description;
-                    item.buyPrice = buyPrice;
-                    item.code = code
-                    item.tags = tags;
+        productsChange.description = description;
+        productsChange.code = code;
+        productsChange.buyPrice = buyPrice;
+        productsChange.sellPrice = sellPrice;
+        productsChange.tags = tags;
 
-                    return item;
-                }
-            }
+        this.products = [...newListProducts , productsChange ];
 
-        )
+        return productsChange;
+    }
 
-        this.products = productsChange;
-
-        return this.products;
+    public  delete(id: string) {
+        const deletedProduct = this.products.find(item => item.id === id);
+        const idDeletedProduct = deletedProduct?.id;
+        // this.products.pop(deletedProduct):Product | undefined;
+        
+        return deletedProduct;
     }
 }
