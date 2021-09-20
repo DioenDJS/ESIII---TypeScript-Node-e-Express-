173c74037638664e357d77778aea4382ca24c6ba
DioenDJS <negociospacheti@gmail.com>
2021-09-20 18:00:48 -0300

    :art: create method alterar Class ClientRepository

--- src/repositories/ClientRepository.ts
+++ src/repositories/ClientRepository.ts
@@ -22,5 +22,19 @@ export default class ClientRepository{
         return newClient;
     }
 
-    
+    public async alterar(name:string, id:string):Promise<Client | undefined> {
+        
+        const confirmClint = this.clients.find(item =>  item.id === id)
+        
+        if(!confirmClint){
+            return undefined;
+        }
+        
+        const clientChange = await this.clients.filter(item =>  item.id !== id? item : item.name = name);
+        
+        this.clients = [];
+        this.clients = clientChange;
+        
+        return confirmClint; 
+    }
 }
