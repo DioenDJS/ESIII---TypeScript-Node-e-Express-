0d87b40eed421e6cab22ef04c618b21e1d01b506
DioenDJS <negociospacheti@gmail.com>
2021-09-25 20:53:01 -0300

    :recycle: configuring decorations on the entity

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -1,12 +1,20 @@
 import { uuid } from 'uuidv4'
 import Product from './Product';
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
 export default class Client{
+
+    @PrimaryGeneratedColumn()
     id: string;
 
+    @Column()
     name: string;
 
+    @Column()
     buyLast: Date;
 
+    @Column()
     purchases: Product[];
 
     constructor({ name, purchases}: Omit<Client, 'id, buyLast'>) {
