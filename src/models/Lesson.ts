7be276fa630445d16aea520392d118cc4ad15dde
DioenDJS <negociospacheti@gmail.com>
2021-09-26 22:27:28 -0300

    :sparkles: create class Lesson

---
+++ src/models/Lesson.ts
@@ -0,0 +1,18 @@
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+
+@Entity()
+export default class Lesson {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    description: string;
+
+    @CreateDateColumn({name: 'created_At'})
+    createAt: Date;
+
+    @UpdateDateColumn({name: 'update_At'})
+    updateAt: Date;
+
+}
