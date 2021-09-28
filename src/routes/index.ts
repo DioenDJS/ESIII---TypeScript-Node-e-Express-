699c7b9236ed683afb1faf68b9d02c5f415e571d
DioenDJS <negociospacheti@gmail.com>
2021-09-27 21:45:06 -0300

    :art: insert router class the routes directory index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import clientRouter from './client.routes';
-import productRouter from './product.routes';
+import classRouter from './class.routes';
+// import clientRouter from './client.routes';
+// import productRouter from './product.routes';
 
 const routes = Router();
 
-routes.use('/products', productRouter);
-routes.use('/clients', clientRouter);
+// routes.use('/products', productRouter);
+// routes.use('/clients', clientRouter);
+routes.use('/class', classRouter);
 export default routes;
